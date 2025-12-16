29d5fa18a24a20a09327edd2dabfdf3b081735fd
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-12-16 14:37:32 +0100

    added invitation accept function

--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,31 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app
 
-# For cost control, you can set the maximum number of containers that can be
-# running at the same time. This helps mitigate the impact of unexpected
-# traffic spikes by instead downgrading performance. This limit is a per-function
-# limit. You can override the limit for each function using the max_instances
-# parameter in the decorator, e.g. @https_fn.on_request(max_instances=5).
-set_global_options(max_instances=10)
+set_global_options(max_instances=2)
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+@firestore_fn.on_document_updated(document="invitations/{inviteId}")
+def on_invite_used(event: firestore_fn.Event):
+    before = event.data.before.to_dict()
+    after = event.data.after.to_dict()
+
+    # detect newly added user
+    new_users = set(after["usedBy"]) - set(before["usedBy"])
+    if not new_users:
+        return
+
+    uid = list(new_users)[0]
+    home_id = after["homeId"]
+
+    # add user to group
+    db.collection("homes").document(home_id).update({
+        "members": firestore.ArrayUnion([uid])
+    })
+
+    # create permissions doc
+    db.collection("homes").document(home_id) \
+      .collection("permissions").document(uid).set({
+        "isOwner": False,
+    })
